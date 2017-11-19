e74065245777b914f5dbb1cb25b7d89c8a6c535e
Yoidea <yoidea.cc@gmail.com>
2017-11-19 21:37:50 +0900

    Update

--- train.py
+++ train.py
@@ -1,17 +1,30 @@
+from argparse import ArgumentParser
 from keras.optimizers import Adam
-from utils.image import load_data
+from utils.image import load_data, to_dirname
 from utils.file import save_model, save_list
 from networks.relu import build_model
 
 
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, default='images')
+    parser.add_argument('-b', '--batch', type=int, default=10)
+    parser.add_argument('-e', '--epoch', type=int, default=3000)
+    return parser.parse_args()
+
+
 def main():
-    (sets, labels, names) = load_data('images/', (64, 64))
+    args = get_args()
+    dirname = to_dirname(args.input)
+    batch = args.batch
+    epochs = args.epoch
+    (sets, labels, names) = load_data(dirname, (64, 64))
     save_list(names, 'names.json')
     model = build_model((64, 64), 3)
     optimizer = Adam(lr=0.0001)
     save_model(model, 'model.json')
     model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-    model.fit(sets, labels, epochs=5000)
+    model.fit(sets, labels, batch_size=batch, epochs=epochs)
     model.save_weights('weights.hdf5')
 
 
